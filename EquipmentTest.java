57fde1d02f811b1ffdb4cf74d679e87ae68c7883
Noah Pierce <noah.j.pierce@ou.edu>
2019-02-08 22:07:30 -0600

    finsihed EquipmentTest class

---
+++ EquipmentTest.java
@@ -0,0 +1,37 @@
+
+public class EquipmentTest
+{
+	
+	public void equipmentConstructorTest() throws AssertException
+	{
+		
+	}
+	
+	public void toStringTest() throws AssertException
+	{
+		
+	}
+	
+	public void getColorTest() throws AssertException
+    {
+    	
+    }
+    
+    public void getNameTest() throws AssertException
+    {
+    	
+    }
+    
+    public void getHeightTest() throws AssertException
+    {
+    	
+    }
+    
+    public void getWeightTest() throws AssertException
+    {
+    	
+    }
+	
+	
+
+}
--- Todo.java
+++ Todo.java
@@ -1,13 +1,16 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 12
+Last Modified_______: 2019-02-08
+OU ID_______________: 113484432
+OU EMAIL____________: noah.j.pierce@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
-
+1. test zoo constructors  
+2. test animal constructors 
+3. test equipment constructors 
+4. check animal methods  
+5. check zoo methods 
+6. finish animal equals methods 
 Estimated Time (minutes):
 1. XXX
 2. XXx
