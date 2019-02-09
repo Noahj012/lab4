a1c6d007ff75b5c7939371fe0333e999d5ff5f59
Noah Pierce <noah.j.pierce@ou.edu>
2019-02-08 20:00:28 -0600

    finished the AnimalTest class, full constructor, gettersTest, and toString

--- AnimalTest.java
+++ AnimalTest.java
@@ -6,6 +6,11 @@
  */
 public class AnimalTest
 {
+	private final String COLOR = "Red";
+	private final String NAME = "Betsy";
+	private final String TO_STRING = "noname, a unknown-colored animal. 185 pounds, 10 inches.\n";
+	private final double HEIGHT = 10;
+	private final double WEIGHT = 185;
 
     /**
      * Test the empty Animal constructor and the getters
@@ -23,5 +28,55 @@ public class AnimalTest
     }
 
     // TODO: test full constructor, getters, and toString
+    
+    public void fullContructorTest() throws AssertException
+    {
+    	// declares cow animal named Betsy
+    	Animal ani = new Animal(COLOR, NAME, HEIGHT, WEIGHT);
+    	
+    	// Tests fully declared animal object
+    	Assert.assertEquals(COLOR, ani.getColor());
+        Assert.assertEquals(NAME, ani.getName());
+        Assert.assertEquals(HEIGHT, ani.getHeight(), 0.01);
+        Assert.assertEquals(WEIGHT, ani.getWeight(), 0.01);
+    }
+    
+    public void getColorTest() throws AssertException
+    {
+    	Animal ani = new Animal(COLOR, "", 0, 0);
+    	
+    	Assert.assertEquals(COLOR, ani.getColor());
+    }
+    
+    public void getNameTest() throws AssertException
+    {
+    	Animal ani = new Animal("", NAME, 0, 0);
+    	
+    	Assert.assertEquals(NAME, ani.getName());
+    }
+    
+    public void getHeightTest() throws AssertException
+    {
+    	Animal ani = new Animal("", "", HEIGHT, 0);
+    	
+    	Assert.assertEquals(HEIGHT, ani.getHeight());
+    }
+    
+    public void getWeightTest() throws AssertException
+    {
+    	Animal ani = new Animal("", "", 0, WEIGHT);
+    	
+    	Assert.assertEquals(WEIGHT, ani.getWeight());
+    }
+    
+    
+    public void toStringTest() throws AssertException
+    {
+    	Animal ani = new Animal();
+    	
+    	Assert.assertEquals(TO_STRING, ani.toString());
+    }
+    
+    
 }
 
